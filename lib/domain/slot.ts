b3fd14796b449ad28b954863f58c9201cbc5d51e
4lessandrodev <alessandroadm@live.com>
2023-12-28 20:49:31 -0300

    feat: added slot

---
+++ lib/domain/slot.ts
@@ -0,0 +1,13 @@
+import { ValueObject } from "rich-domain";
+
+type Props = {
+    /**
+     * @todo implementar propriedades
+     */
+}
+
+export default class Slot extends ValueObject<Props> {
+    /**
+     * @todo implementar métodos
+     */
+}
