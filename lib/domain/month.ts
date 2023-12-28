a7a935ab50aa11d0f1d61a0a866b889397f8e337
4lessandrodev <alessandroadm@live.com>
2023-12-28 20:39:14 -0300

    feat: added month

---
+++ lib/domain/month.ts
@@ -0,0 +1,13 @@
+import { ValueObject } from "rich-domain";
+
+type Props = {
+    /**
+     * @todo implementar propriedades
+     */
+}
+
+export default class Month extends ValueObject<Props> {
+    /**
+     * @todo implementar métodos
+     */
+}
