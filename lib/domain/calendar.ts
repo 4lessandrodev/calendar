5a20ca9fea160b01ff37fa1765a1c644d485b5bc
4lessandrodev <alessandroadm@live.com>
2023-12-28 20:22:06 -0300

    feat: added calendar aggregate

---
+++ lib/domain/calendar.ts
@@ -0,0 +1,13 @@
+import { Aggregate } from "rich-domain"
+
+type Props = {
+    /**
+     * @todo implementar propriedades
+     */
+}
+
+export default class Calendar extends Aggregate<Props> {
+    /**
+     * @todo implementar métodos
+     */
+}
