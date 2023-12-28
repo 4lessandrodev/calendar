b84bedea05acbcf1e68a5bd6e98dda596aef34fa
4lessandrodev <alessandroadm@live.com>
2023-12-28 20:43:04 -0300

    feat: added week

---
+++ lib/domain/week.ts
@@ -0,0 +1,13 @@
+import { ValueObject } from "rich-domain";
+
+type Props = {
+    /**
+     * @todo implementar propriedades
+     */
+}
+
+export default class Week extends ValueObject<Props> {
+    /**
+     * @todo implementar métodos
+     */
+}
