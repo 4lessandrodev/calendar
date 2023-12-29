aa6e1fd5191ebedf784024107f61d8f6bbe8d92d
4lessandrodev <alessandroadm@live.com>
2023-12-28 23:48:27 -0300

    feat: added list

--- lib/domain/list.ts
+++ lib/domain/list.ts
@@ -1,11 +1,24 @@
 export default class List<T> {
+
     constructor(private items: Array<T>) { }
-    
+
     add(item: T): void {
         this.items.push(item);
     }
 
-    remove(query: (item: T) => void): void {
-        this.items = this.items.filter(query)
+    remove(item: T): void {
+        this.items = this.items.filter(
+            (element): boolean => JSON.stringify(element) !== JSON.stringify(item)
+        )
     }
 }
+
+const list = new List<string>([]);
+
+list.add('oi')
+
+console.log(list);
+
+list.remove('oi')
+
+console.log(list)
