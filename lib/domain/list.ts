45df343ccd7bd06f55474fc4aaa19340a225e30f
4lessandrodev <alessandroadm@live.com>
2023-12-28 23:39:40 -0300

    feat: added list

--- lib/domain/list.ts
+++ lib/domain/list.ts
@@ -1,5 +1,11 @@
 export default class List<T> {
-    /**
-     * @todo implementar métodos
-     */
+    constructor(private items: Array<T>) { }
+    
+    add(item: T): void {
+        this.items.push(item);
+    }
+
+    remove(query: (item: T) => void): void {
+        this.items = this.items.filter(query)
+    }
 }
